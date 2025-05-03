c498e2330f6971601cc17889c12095de0496ef0e
Ana <89231022@student.upr.si>
2025-05-03 13:05:57 +0200

    First commit:Choosing mode of execution

--- src/main/java/org/nft/Main.java
+++ src/main/java/org/nft/Main.java
@@ -20,32 +20,57 @@ public class Main {
         );
 
         Scanner s=new Scanner(System.in);
+
+        //choose the method for execution
+        System.out.println("Choose execution mode:");
+        System.out.println("1-Sequential execution");
+        System.out.println("2-Parallel execution");
+        System.out.println("3-Distributed execution");
+        int mode=s.nextInt();
+
+        if(mode<1 || mode>3){
+            System.out.println("Invalid mode. Please provide correct mode");
+            s.close();
+            return;
+        }
+
         System.out.println("Enter the maximal depth of the traversal:");
         int maxDepth=s.nextInt();
         s.close();
 
         long start=System.nanoTime();
 
-        //read file
-        readFile reader=new readFile(file,maxDepth,cexFile);
-        Map<String, List<Graph.Edge>> adjList=reader.read();
+        //cases for mode
+        switch(mode){
+            case 1:
+                ExecutionMode.Sequential(file,maxDepth,cexFile,output);
+                break;
+            case 2:
+                ExecutionMode.Parallel(file,maxDepth,cexFile,output);
+                break;
+            case 3:
+                ExecutionMode.Distributed(file,maxDepth,cexFile,output);
+                break;
+        }
 
-        //print the adj list
-        reader.printAdjacencyList(adjList);
-
-        //build the linkability network
-        Map<String, List<Graph.Edge>> linkabilityNetwork = reader.buildLinkabilitynetwork(adjList);
-
-        //write the linkability network to csv file
-        reader.writeLinkabilityNetwork(adjList,output);
-        System.out.println("Linkability Network written to: " + output);
-
-
-
-        //test
-
-        //print the count of links
-        reader.printLinkWeightCount(linkabilityNetwork);
+//        //read file
+//        readFile reader=new readFile(file,maxDepth,cexFile);
+//        Map<String, List<Graph.Edge>> adjList=reader.read();
+//
+//
+//
+//        //print the adj list
+//        reader.printAdjacencyList(adjList);
+//
+//        //build the linkability network
+//        Map<String, List<Graph.Edge>> linkabilityNetwork = reader.buildLinkabilitynetwork(adjList);
+//
+//        //write the linkability network to csv file
+//        reader.writeLinkabilityNetwork(adjList,output);
+//        System.out.println("Linkability Network written to: " + output);
+//
+//        //print the count of links
+//        reader.printLinkWeightCount(linkabilityNetwork);
 
         long end=System.nanoTime();
         System.out.println("Time to compute the linkability network: "+(end-start)/1000000000+"s");
